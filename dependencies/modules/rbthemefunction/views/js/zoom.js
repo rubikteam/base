3449bf73ee444efda3d7b77fedc5d28a61d7b9fc
long22041992 <danghoanglong22041992@gmail.com>
2019-12-07 10:04:30 +0700

    mobile thumb

--- dependencies/modules/rbthemefunction/views/js/zoom.js
+++ dependencies/modules/rbthemefunction/views/js/zoom.js
@@ -64,6 +64,50 @@ function initSlick(check)
 	  			autoplaySpeed: rb_slick['autoplay'] == 1 && rb_slick['autospeed'] != '' ? rb_slick['autospeed'] : 1000,
 	  			asNavFor: '.product-cover',
 	  			focusOnSelect: true,
+	  			responsive: [
+		            {
+		                breakpoint: 1200,
+		                settings: {
+		                    slidesToShow: 3,
+		                    slidesToScroll: 1,
+		                }
+		            },
+		            {
+		                breakpoint: 992,
+		                settings: {
+		                    slidesToShow: 2,
+		                    slidesToScroll: 1
+		                }
+		            },
+		            {
+		                breakpoint: 768,
+		                settings: {
+		                    slidesToShow: 2,
+		                    slidesToScroll: 1
+		                }
+		            },
+		            {
+		                breakpoint: 576,
+		                settings: {
+		                    slidesToShow: 1,
+		                    slidesToScroll: 1
+		                }
+		            },
+		            {
+		                breakpoint: 480,
+		                settings: {
+		                    slidesToShow: 2,
+		                    slidesToScroll: 1
+		                }
+		            },
+		            {
+		            breakpoint: 400,
+		                settings: {
+		                    slidesToShow: 1,
+		                    slidesToScroll: 1
+		                }
+		            }
+        		]
 			});
 		}
 	}
